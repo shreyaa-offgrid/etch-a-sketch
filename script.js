416e325b4e2f0818724e71a9c161a577abded8de
Shreya Singh <143871325+shreyaa-offgrid@users.noreply.github.com>
2025-08-02 17:30:57 +0000

    Add new grid logic

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 //create 16*16 grid
 let n = 16;
-for(let i=0;i<n;i++){
-    let row = document.createElement("div");
-    row.classList.add("row");
+createGrid(n);
+const button = document.querySelector("button");
+button.addEventListener("click", function () {
+    n = prompt("Enter no. of boxes (n) per side for (nxn) grid");
+    while(n>100){
+        n = prompt("Too large! Please enter a numer less than 100")
+    }
     let container = document.querySelector(".container");
-    container.appendChild(row);
-    for(let j = 0;j<n;j++){
-        let box = document.createElement("div");
-        box.classList.add("box");
-        row.appendChild(box);
+    container.innerHTML="";
+    createGrid(n);
+});
+
+function createGrid(n) {
+    const boxSide = 960/n;
+    for (let i = 0; i < n; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        let container = document.querySelector(".container");
+        container.appendChild(row);
+        for (let j = 0; j < n; j++) {
+            let box = document.createElement("div");
+            box.classList.add("box");
+            row.appendChild(box);
+            box.style.height = boxSide+"px";
+            box.style.width = boxSide+"px";
+        }
+    }
+
+    const boxes = document.querySelectorAll(".box");
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener("mouseenter", function (e) {
+            let box = e.target;
+            console.log(box);
+            box.style.backgroundColor = "green";
+        });
     }
 }
-const boxes = document.querySelectorAll(".box");
-for(let i=0;i<boxes.length;i++){
-    boxes[i].addEventListener("mouseenter", function(e){
-        let box = e.target;
-        console.log(box);
-        box.style.backgroundColor = "green";
-    });
-}
+
